cb3dfb5062138788e540d400f630a552d043f9b1
luis233-byte <you@example.com>
2021-05-10 19:49:48 -0500

    Actualización de proyecto, responsive monitores

---
+++ js/sweetAlert.js
@@ -0,0 +1,510 @@
+/* alerta del boton */
+function btn_on() {
+  swal.fire({
+    icon: "success",
+    padding: "25px",
+    width: "40%",
+    position: "center",
+    title: " Alerta !",
+    text: "Usted a Ejecutado una accion",
+    html:
+      "<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris arcu enim, fringilla non suscipit v </p>",
+    background: "azure",
+    backdrop: true,
+    allowOutsideClick: false,
+    allowEscape: false,
+    allowEnterKey: false,
+  });
+}
+
+function btn_off() {
+  swal.fire({
+    icon: "warning"  ,
+    padding: "25px",
+    width: "60%",
+    position: "center",
+    title: " Alerta !",
+    text: "Usted a Ejecutado una accion",
+    html:
+      "<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris arcu enim, fringilla non suscipit v </p>",
+    background: "gray",
+    
+    backdrop: true,
+    allowOutsideClick: false,
+    allowEscape: false,
+    allowEnterKey: false,
+  });
+}
+
+// Alertas botones off
+
+function btn_off() {
+  swal.fire({
+    icon: "warning"  ,
+    padding: "25px",
+    width: "60%",
+    position: "center",
+    title: " Alerta !",
+    text: "Usted a Ejecutado una accion",
+    html:
+      "<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris arcu enim, fringilla non suscipit v </p>",
+      background: "azure",
+    
+    backdrop: true,
+    allowOutsideClick: false,
+    allowEscape: false,
+    allowEnterKey: false,
+  });
+}
+
+function btn_off1() {
+  swal.fire({
+    icon: "warning"  ,
+    padding: "25px",
+    width: "60%",
+    position: "center",
+    title: " Alerta !",
+    text: "Usted a Ejecutado una accion",
+    html:
+      "<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris arcu enim, fringilla non suscipit v </p>",
+      background: "azure",
+    
+    backdrop: true,
+    allowOutsideClick: false,
+    allowEscape: false,
+    allowEnterKey: false,
+
+  });
+}
+
+function btn_off2() {
+  swal.fire({
+    icon: "warning"  ,
+    padding: "25px",
+    width: "60%",
+    position: "center",
+    title: " Alerta !",
+    text: "Usted a Ejecutado una accion",
+    html:
+      "<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris arcu enim, fringilla non suscipit v </p>",
+      background: "azure",
+    
+    backdrop: true,
+    allowOutsideClick: false,
+    allowEscape: false,
+    allowEnterKey: false,
+  });
+}
+
+function btn_off3() {
+  swal.fire({
+    icon: "warning"  ,
+    padding: "25px",
+    width: "60%",
+    position: "center",
+    title: " Alerta !",
+    text: "Usted a Ejecutado una accion",
+    html:
+      "<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris arcu enim, fringilla non suscipit v </p>",
+    background: "azure",
+    
+    backdrop: true,
+    allowOutsideClick: false,
+    allowEscape: false,
+    allowEnterKey: false,
+  });
+}
+
+// Alertas botones msgn-1 columna 1
+
+function btn_msgn1() {
+  swal.fire({
+    icon: "warning"  ,
+    padding: "25px",
+    width: "60%",
+    position: "center",
+    title: " Alerta !",
+    text: "Usted a Ejecutado una accion",
+    html:
+      "<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris arcu enim, fringilla non suscipit v </p>",
+    background: "azure",
+    
+    backdrop: true,
+    allowOutsideClick: false,
+    allowEscape: false,
+    allowEnterKey: false,
+  });
+
+}
+
+function btn_msgn11() {
+  swal.fire({
+    icon: "warning"  ,
+    padding: "25px",
+    width: "60%",
+    position: "center",
+    title: " Alerta !",
+    text: "Usted a Ejecutado una accion",
+    html:
+      "<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris arcu enim, fringilla non suscipit v </p>",
+    background: "azure",
+    
+    backdrop: true,
+    allowOutsideClick: false,
+    allowEscape: false,
+    allowEnterKey: false,
+  });
+}
+
+function btn_msgn111() {
+  swal.fire({
+    icon: "warning"  ,
+    padding: "25px",
+    width: "60%",
+    position: "center",
+    title: " Alerta !",
+    text: "Usted a Ejecutado una accion",
+    html:
+      "<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris arcu enim, fringilla non suscipit v </p>",
+    background: "azure",
+    
+    backdrop: true,
+    allowOutsideClick: false,
+    allowEscape: false,
+    allowEnterKey: false,
+  });
+}
+
+// Alertas botones msgn-2
+
+function btn_msgn2() {
+  swal.fire({
+    icon: "warning"  ,
+    padding: "25px",
+    width: "60%",
+    position: "center",
+    title: " Alerta !",
+    text: "Usted a Ejecutado una accion",
+    html:
+      "<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris arcu enim, fringilla non suscipit v </p>",
+    background: "azure",
+    
+    backdrop: true,
+    allowOutsideClick: false,
+    allowEscape: false,
+    allowEnterKey: false,
+  });
+}
+
+function btn_msgn22() {
+  swal.fire({
+    icon: "warning"  ,
+    padding: "25px",
+    width: "60%",
+    position: "center",
+    title: " Alerta !",
+    text: "Usted a Ejecutado una accion",
+    html:
+      "<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris arcu enim, fringilla non suscipit v </p>",
+    background: "azure",
+    
+    backdrop: true,
+    allowOutsideClick: false,
+    allowEscape: false,
+    allowEnterKey: false,
+  });
+}
+
+function btn_msgn222() {
+  swal.fire({
+    icon: "warning"  ,
+    padding: "25px",
+    width: "60%",
+    position: "center",
+    title: " Alerta !",
+    text: "Usted a Ejecutado una accion",
+    html:
+      "<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris arcu enim, fringilla non suscipit v </p>",
+    background: "azure",
+    
+    backdrop: true,
+    allowOutsideClick: false,
+    allowEscape: false,
+    allowEnterKey: false,
+  });
+}
+
+
+// Alertas botones msgn-3
+
+function btn_msgn3() {
+  swal.fire({
+    icon: "warning"  ,
+    padding: "25px",
+    width: "60%",
+    position: "center",
+    title: " Alerta !",
+    text: "Usted a Ejecutado una accion",
+    html:
+      "<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris arcu enim, fringilla non suscipit v </p>",
+    background: "azure",
+    
+    backdrop: true,
+    allowOutsideClick: false,
+    allowEscape: false,
+    allowEnterKey: false,
+  });
+}
+
+function btn_msgn33() {
+  swal.fire({
+    icon: "warning"  ,
+    padding: "25px",
+    width: "60%",
+    position: "center",
+    title: " Alerta !",
+    text: "Usted a Ejecutado una accion",
+    html:
+      "<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris arcu enim, fringilla non suscipit v </p>",
+    background: "azure",
+    
+    backdrop: true,
+    allowOutsideClick: false,
+    allowEscape: false,
+    allowEnterKey: false,
+  });
+}
+
+function btn_msgn333() {
+  swal.fire({
+    icon: "warning"  ,
+    padding: "25px",
+    width: "60%",
+    position: "center",
+    title: " Alerta !",
+    text: "Usted a Ejecutado una accion",
+    html:
+      "<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris arcu enim, fringilla non suscipit v </p>",
+    background: "azure",
+    
+    backdrop: true,
+    allowOutsideClick: false,
+    allowEscape: false,
+    allowEnterKey: false,
+  });
+
+}
+
+
+
+// Alertas botones msgn-4
+
+function btn_msgn4() {
+  swal.fire({
+    icon: "warning"  ,
+    padding: "25px",
+    width: "60%",
+    position: "center",
+    title: " Alerta !",
+    text: "Usted a Ejecutado una accion",
+    html:
+      "<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris arcu enim, fringilla non suscipit v </p>",
+    background: "azure",
+    
+    backdrop: true,
+    allowOutsideClick: false,
+    allowEscape: false,
+    allowEnterKey: false,
+  });
+}
+
+function btn_msgn44() {
+  swal.fire({
+    icon: "warning"  ,
+    padding: "25px",
+    width: "60%",
+    position: "center",
+    title: " Alerta !",
+    text: "Usted a Ejecutado una accion",
+    html:
+      "<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris arcu enim, fringilla non suscipit v </p>",
+    background: "azure",
+    
+    backdrop: true,
+    allowOutsideClick: false,
+    allowEscape: false,
+    allowEnterKey: false,
+  });
+}
+
+function btn_msgn444() {
+  swal.fire({
+    icon: "warning"  ,
+    padding: "25px",
+    width: "60%",
+    position: "center",
+    title: " Alerta !",
+    text: "Usted a Ejecutado una accion",
+    html:
+      "<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris arcu enim, fringilla non suscipit v </p>",
+    background: "azure",
+    
+    backdrop: true,
+    allowOutsideClick: false,
+    allowEscape: false,
+    allowEnterKey: false,
+  });
+}
+
+// alertas botones sent
+
+function sent_1() {
+  const swalWithBootstrapButtons = Swal.mixin({
+    customClass: {
+      confirmButton:
+           'btn btn-success',
+      cancelButton: 'btn btn-danger'
+    },
+    buttonsStyling: false
+  })
+  
+  swalWithBootstrapButtons.fire({
+    title: 'Esta a punto de enviar un mensaje desea continuar ?',
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonText: 'Si, Continuar',
+    cancelButtonText: 'No, Cancelar',
+    reverseButtons: true
+  }).then((result) => {
+    if (result.isConfirmed) {
+      swalWithBootstrapButtons.fire(
+        'Deleted!',
+        'Your file has been deleted.',
+        'success'
+      )
+    } else if (
+      /* Read more about handling dismissals below */
+      result.dismiss === Swal.DismissReason.cancel
+    ) {
+      swalWithBootstrapButtons.fire(
+        'Cancelled',
+        'Your imaginary file is safe :)',
+        'error'
+      )
+    }
+  })
+}
+
+function sent_2() {
+  const swalWithBootstrapButtons = Swal.mixin({
+    customClass: {
+      confirmButton:
+           'btn btn-success',
+      cancelButton: 'btn btn-danger'
+    },
+    buttonsStyling: false
+  })
+  
+  swalWithBootstrapButtons.fire({
+    title: 'Esta a punto de enviar un mensaje desea continuar ?',
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonText: 'Si, Continuar',
+    cancelButtonText: 'No, Cancelar',
+    reverseButtons: true
+  }).then((result) => {
+    if (result.isConfirmed) {
+      swalWithBootstrapButtons.fire(
+        'Deleted!',
+        'Your file has been deleted.',
+        'success'
+      )
+    } else if (
+      /* Read more about handling dismissals below */
+      result.dismiss === Swal.DismissReason.cancel
+    ) {
+      swalWithBootstrapButtons.fire(
+        'Cancelled',
+        'Your imaginary file is safe :)',
+        'error'
+      )
+    }
+  })
+  
+}
+
+function sent_3() {
+  const swalWithBootstrapButtons = Swal.mixin({
+    customClass: {
+      confirmButton:
+           'btn btn-success',
+      cancelButton: 'btn btn-danger'
+    },
+    buttonsStyling: false
+  })
+  
+  swalWithBootstrapButtons.fire({
+    title: 'Esta a punto de enviar un mensaje desea continuar ?',
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonText: 'Si, Continuar',
+    cancelButtonText: 'No, Cancelar',
+    reverseButtons: true
+  }).then((result) => {
+    if (result.isConfirmed) {
+      swalWithBootstrapButtons.fire(
+        'Deleted!',
+        'Your file has been deleted.',
+        'success'
+      )
+    } else if (
+      /* Read more about handling dismissals below */
+      result.dismiss === Swal.DismissReason.cancel
+    ) {
+      swalWithBootstrapButtons.fire(
+        'Cancelled',
+        'Your imaginary file is safe :)',
+        'error'
+      )
+    }
+  })
+}
+
+function sent_4() {
+  const swalWithBootstrapButtons = Swal.mixin({
+    customClass: {
+      confirmButton:
+           'btn btn-success',
+
+      cancelButton: 'btn btn-danger'
+    },
+    buttonsStyling: false
+  })
+  
+  swalWithBootstrapButtons.fire({
+    title: 'Esta a punto de enviar un mensaje desea continuar ?',
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonText: 'Si, Continuar',
+    cancelButtonText: 'No, Cancelar',
+    reverseButtons: true
+  }).then((result) => {
+    if (result.isConfirmed) {
+      swalWithBootstrapButtons.fire(
+        'Enviado !',
+        'Su mensaje ha sido enviado.',
+        'success'
+      )
+    } else if (
+      /* Read more about handling dismissals below */
+      result.dismiss === Swal.DismissReason.cancel
+    ) {
+      swalWithBootstrapButtons.fire(
+        'Cancelado !',
+        'Usted ha cancelado el envio del mensaje :)',
+        'error'
+      )
+    }
+  })
+}
+
